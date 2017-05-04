256d48465d4c85c0af4c50c90c9d745d4d8ab211
Ryan Leto <ryjleto@gmail.com>
2017-05-04 15:48:40 -0600

    omit curly apostrophe and quote punctuation

--- lib/engtagger.rb
+++ lib/engtagger.rb
@@ -642,7 +642,9 @@ class EngTagger
     text = text.gsub(/--+/o, " - ") # Convert and separate dashes
     text = text.gsub(/,(?!\d)/o, " , ") # Shift commas off everything but numbers
     text = text.gsub(/:/o, " : ") # Shift semicolons off
-    text = text.gsub(/'/o, " ' ") # Shift apostrophes off
+    text = text.gsub(/'/o, " ' ") # Shift single quote off
+    text = text.gsub!(/[”“]/, ' ') # Shift curly quotes off
+    text = text.gsub!(/[‘’]/, " ") # Shift curly apostrophes off
     text = text.gsub(/\.(?!\d)/o, " . ") # Shift periods off everything but numbers
     text = text.gsub(/(\.\.\.+)/o){" " + $1 + " "} # Shift ellipses off
     text = text.gsub(/([\(\[\{\}\]\)])/o){" " + $1 + " "} # Shift off brackets
